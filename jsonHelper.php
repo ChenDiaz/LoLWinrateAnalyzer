6891e5008f6f03641e402f74371260dddf5586cb
Scott Chen <scottchen625@gmail.com>
2015-11-24 11:47:17 -0800

    Enforce the coding practice of maximum 120 characters per line
    I broke a couple of the lines in jsonHelper.php into two lines.
    It's just bad practice to have too many characters in one line--
    Here's a good set of guidelines.
    https://www.drupal.org/node/935284

--- jsonHelper.php
+++ jsonHelper.php
@@ -1,7 +1,8 @@
 <?php
 	//General function for getting and decoding user JSON data
 	function getSummonerId($user) {
-		$userUrl = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" . $user . "?api_key=5416b2e6-d64c-4826-8b68-3cb6ee7489ff";
+		$userUrl = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" . $user 
+				. "?api_key=5416b2e6-d64c-4826-8b68-3cb6ee7489ff";
         $userJson = file_get_contents($userUrl);
         $userData = json_decode($userJson, true);
         $userId = $userData[$user]['id'];
@@ -10,7 +11,8 @@
 	}
 
 	function getMatchList($userId) {
-		$userMatchIdUrl = "https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/" . $userId . "?rankedQueues=RANKED_SOLO_5x5&beginIndex=0&endIndex=12&api_key=5416b2e6-d64c-4826-8b68-3cb6ee7489ff";
+		$userMatchIdUrl = "https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/" . $userId 
+				. "?rankedQueues=RANKED_SOLO_5x5&beginIndex=0&endIndex=12&api_key=5416b2e6-d64c-4826-8b68-3cb6ee7489ff";
         $userMatchIdJSON = file_get_contents($userMatchIdUrl);
         $userMatchList = json_decode($userMatchIdJSON, true);
 
@@ -18,7 +20,8 @@
 	}
 
 	function getChampionName($championId) {
-		$championIdURL = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" . $championId . "?api_key=5416b2e6-d64c-4826-8b68-3cb6ee7489ff";
+		$championIdURL = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" . $championId 
+				. "?api_key=5416b2e6-d64c-4826-8b68-3cb6ee7489ff";
 		$championIdJSON = file_get_contents($championIdURL);
 		$championIdData = json_decode($championIdJSON);
 		$championName = $championIdData->name;
@@ -29,7 +32,8 @@
 	function matchesWon($numberOfMatches, $userMatchList, $userId) {
 		$matchWins = 0;
         for ($i = 0; $i < $numberOfMatches; $i++) {
-            $userMatchURL = "https://na.api.pvp.net/api/lol/na/v2.2/match/" . $userMatchList["matches"][$i]["matchId"] . "?api_key=451d171b-aefb-4b11-ba80-212cbbcc9d79";
+            $userMatchURL = "https://na.api.pvp.net/api/lol/na/v2.2/match/" . $userMatchList["matches"][$i]["matchId"] 
+            		. "?api_key=451d171b-aefb-4b11-ba80-212cbbcc9d79";
             $userMatchJSON = file_get_contents($userMatchURL);
             $userMatch = json_decode($userMatchJSON, true);
 
